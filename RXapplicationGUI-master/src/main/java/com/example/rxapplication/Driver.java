583470d3ab1fa78402c597450d4bfbdcffd2a83e
hammaadrizwan <hammaadrizwan@gmail.com>
2023-03-06 20:47:49 +0530

    VCT designed

--- RXapplicationGUI-master/src/main/java/com/example/rxapplication/Driver.java
+++ RXapplicationGUI-master/src/main/java/com/example/rxapplication/Driver.java
@@ -41,7 +41,7 @@ public class Driver implements Serializable {// same as the Race class, the driv
 
     //inorder to set values it should be in the Serializable data type
     public void setFname(SerializableSimpleStringProperty fname){
-        this.fname=fname;
+        this.fname=fname;//gets the value from the user and replaces it with the value which was stored previously in the object
     }
     public void setLname(SerializableSimpleStringProperty lname){
         this.lname=lname;
